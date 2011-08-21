d766ad8814669a9120f33a3c48b51da73df6cb00
namakesugi <info@namakesugi.net>
2011-08-22 00:47:55 +0900

    refactoring

--- lib/privacy_mask_tools/email_matcher.rb
+++ lib/privacy_mask_tools/email_matcher.rb
@@ -3,41 +3,73 @@
 # Email判定用モジュール
 module PrivacyMaskTools::EmailMatcher
 
-  # RFC 2822 と RFC 3696 に対応した正規表現を生成します
-  #   概ね対応しているレベルであり、完全に対応しているわけではありません
-  #
-  # validates_email_format_ofを参考にしています
-  # @see https://github.com/alexdunae/validates_email_format_of
-  # TODO chache
-  def make_rfc_regexp
-    special  = Regexp.escape('!#$%&\'*-/=?+-^_`{|}~')
-    quoted_special = Regexp.escape('()<>[]:;@, ')
-    unquoted = "([[0-9A-Za-z]#{special}]+[\.]+)*[[0-9A-Za-z]#{special}+]+"
-    quoted   = "\"[0-9A-Za-z#{special}#{quoted_special}\.]*\""
-    domain   = "((\w+\-+[^_])|(\w+\.[-a-z0-9]*))*([-a-z0-9\.]{1,63})\.[a-z]{2,6}(?:\.[a-z]{2,6})?"
-    Regexp.new('(('+ unquoted + '|' + quoted + '+)@(' + domain + '))')
-  end
-
+  # RFC5322形式に近いメールアドレスが存在するかチェックします
+  # @param [String] 探索対象文字列
+  # @return [Boolean]
   def has_rfc_email?(text)
     !text.match(make_rfc_regexp).nil?
   end
 
+  # RFC5322形式に近いメールアドレスを抽出します
+  # @param [String] 探索対象文字列
+  # @return [Array]
   def pick_rfc_email(text)
     [].tap { |o|
       text.scan(make_rfc_regexp) { |f| o << f[0] }
     }
   end
 
-  def has_email?(text, jargon=false)
+  # RFC5322形式に近いメールアドレスをマスキングします
+  # @param [String] 探索対象文字列
+  # @param [String] 置換文字列
+  # @return [String] 置換後の文字列
+  def rfc_email_masking(text, words="***")
+    text.dup.tap {|o| text.scan(make_rfc_regexp) { |f| o.sub!(f[0], words) } }
   end
 
-  def has_mobile_email?(text, jargon=false)
+  # ----- メールアドレス正規表現メソッド -----
+
+  # RFC5322 に対応した正規表現を生成します
+  #   概ね対応しているレベルであり、完全に対応しているわけではありません
+  #
+  # validates_email_format_ofを参考にしています
+  # @see https://github.com/alexdunae/validates_email_format_of
+  # TODO chache
+  def make_rfc_regexp
+    Regexp.new('(('+ unquoted_regexp + '|' + quoted_regexp + '+)@(' + domain_regexp + '))')
   end
 
-  def email_masking(text, word='*', jargon=false)
+  # メールアドレスのローカル部で使用可能な特殊記号
+  # @return [String] エスケープ処理された特殊記号
+  def special_char
+    Regexp.escape('!#$%&\'*-/=?+-^_`{|}~')
   end
 
-  def mobile_email_masking(text, word='*', jargon=false)
+  # メールアドレスのローカル部のダブルクウォーテーションで囲まれた部分で使用可能な特殊記号
+  # @return [String] エスケープ処理された特殊記号
+  def quoted_special_char
+    Regexp.escape('()<>[]:;@, ')
+  end
+
+  # メールアドレスのローカル部の正規表現文字列
+  #   このままだと文字列ですので、Regexp.new等で正規表現に変換する必要があります
+  # @return [String] 正規表現文字列
+  def unquoted_regexp
+    "([[0-9A-Za-z]#{special_char}]+[\.]+)*[[0-9A-Za-z]#{special_char}+]+"
+  end
+
+  # メールアドレスのローカル部において ダブルクウォーテーションで囲まれている部分の正規表現文字列
+  #   このままだと文字列ですので、Regexp.new等で正規表現に変換する必要があります
+  # @return [String] 正規表現文字列
+  def quoted_regexp
+    "\"[0-9A-Za-z#{special_char}#{quoted_special_char}\.]*\""
+  end
+
+  # メールアドレスのドメイン部の正規表現文字列
+  #   このままだと文字列ですので、Regexp.new等で正規表現に変換する必要があります
+  # @return [String] 正規表現文字列
+  def domain_regexp
+    "((\w+\-+[^_])|(\w+\.[-a-z0-9]*))*([-a-z0-9\.]{1,63})\.[a-z]{2,6}(?:\.[a-z]{2,6})?"
   end
 end
 
--- spec/privacy_mask_tools/email_matcher_spec.rb
+++ spec/privacy_mask_tools/email_matcher_spec.rb
@@ -10,13 +10,12 @@ describe PrivacyMaskTools::EmailMatcher do
   let!(:matcher) { Obj.new }
 
   # @see http://ja.wikipedia.org/wiki/%E3%83%A1%E3%83%BC%E3%83%AB%E3%82%A2%E3%83%89%E3%83%AC%E3%82%B9
-  let!(:rfc_emails) { [].tap { |o|
-    o << "Abc.123@example.com"
-    o << "user+mailbox/department=shipping@example.com"
-    o << "!#$\%&'*+-/=?^_`.{|}~@example.com"
-    o << "\"Abc@def\"@example.com"
-    o << "\"Fred Bloggs\"@example.com" #    o << "\"Joe.\\\\Blow\"@example.com" # これは諦め
-    }
+  let!(:rfc_emails) { ["Abc.123@example.com",
+    "user+mailbox/department=shipping@example.com",
+    "!#$\%&'*+-/=?^_`.{|}~@example.com",
+    "\"Abc@def\"@example.com",
+    "\"Fred Bloggs\"@example.com"]
+    # "\"Joe.\\\\Blow\"@example.com" # これは諦め
   }
   describe ".has_rfc_email?" do
     context "正しいEmailの場合" do
@@ -44,6 +43,53 @@ describe PrivacyMaskTools::EmailMatcher do
       its([3]) { should eql rfc_emails[3] }
       its([4]) { should eql rfc_emails[4] }
     end
+
+    context "Emailが含まれていない場合" do
+      subject { matcher.pick_rfc_email("testtest") }
+      its(:size) { should eql 0 }
+    end
+  end
+
+  describe ".rfc_email_masking" do
+    context "正しいEmailが1つだけ含まれている場合" do
+      subject { matcher.rfc_email_masking("私のメールアドレスは#{rfc_emails[0]}です") }
+      it { should eql "私のメールアドレスは***です" }
+    end
+
+    context "正しいEmailが2つ以上含まれている場合" do
+      subject { matcher.rfc_email_masking(rfc_emails.join("　デス\r\nよ"), "+++") }
+      it { should eql (["+++"]*5).join("　デス\r\nよ") }
+    end
+
+    context "Emailが含まれていない場合" do
+      subject { matcher.rfc_email_masking("testtest＠hoge.com") }
+      it { should eql "testtest＠hoge.com" }
+    end
+  end
+
+  describe ".special_char" do
+    subject { matcher.special_char }
+    it { should be_kind_of(String) }
+  end
+
+  describe ".quoted_special_char" do
+    subject { matcher.quoted_special_char }
+    it { should be_kind_of(String) }
+  end
+
+  describe ".unquoted_regexp" do
+    subject { matcher.special_char }
+    it { should be_kind_of(String) }
+  end
+
+  describe ".quoted_regexp" do
+    subject { matcher.quoted_regexp }
+    it { should be_kind_of(String) }
+  end
+
+  describe ".domain_regexp" do
+    subject { matcher.domain_regexp }
+    it { should be_kind_of(String) }
   end
 end
 
